598a4cc3309aad857665c13053fea5a745e72c97
naiya <khiranay99@gmail.com>
2026-04-26 22:50:00 +0700

    fresh start with lfs

---
+++ Assets/Scripts/home_script.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class home_script : MonoBehaviour
+{
+    
+    public void menu_utama()
+    {
+        SceneManager.LoadScene("indah");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
